7b8951b64ff103856d454c114ee90be142f3dde2
Estela Martinez <emargar1@epsa.upv.es>
2025-01-05 15:15:33 +0100

    Clase TalbeEntry modificada para BSTreeDict

--- TableEntry.h
+++ TableEntry.h
@@ -22,6 +22,14 @@ class TableEntry {
 			    return te1.key != te2.key;
 		    }
 
+		    friend bool operator<(const TableEntry<V> &te1, const TableEntry<V> &te2){
+			    return te1.key < te2.key;
+		    }
+
+		    friend bool operator>(const TableEntry<V> &te1, const TableEntry<V> &te2){
+			    return te1.key > te2.key;
+		    }
+
 		    friend std::ostream& operator<<(std::ostream &out, const TableEntry<V> &te){
 			    out << "[Clave -> " << te.key << ", Valor -> " << te.value << "]";
 			    return out;
